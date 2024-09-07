f7e186bc210a44eb296edd20b7e35f5a2c08a846
Rômulo Goes <romulo.fg99@gmail.com>
2024-09-07 15:09:52 -0300

    3º Commit - Commit com a aula 254 finalizada

--- Seção 5/aula254.py
+++ Seção 5/aula254.py
@@ -1,7 +1,39 @@
 """Enum -> Enumerações
 
 Enumerações na programação, são usadas em ocasiões onde temos um determinado
-número de coisas."""
+número de coisas para escolher.
 
-def mover(direcao):
-    ...
+Enums têm membros e seus valores são constantes.
+
+Enums em python:
+    - são um conjunto de nomes simbólicos (membros) ligados a valores únicos
+    - podem ser iterados para retornar seus membros canônicos na ordem de definição
+
+enum.Enum é a superclasse para suas enumerações. Mas também pode ser usada
+diretamente (mesmo assim, Enums não são classes normais em Python).
+Você poderá usar seu Enum com type annotations, com isinstance e
+outras coisas relacionadas com tipo.
+Para obter os dados:
+membro = Classe(valor), Classe['chave']
+chave = Classe.chave.name
+valor = Classe.chave.value
+"""
+import enum
+
+class Direcoes(enum.Enum):
+    ESQUERDA = enum.auto()
+    DIREITA = enum.auto()
+
+direcao = Direcoes
+# Direcoes = enum.Enum('Direcoes', ['ESQUERDA', 'DIREITA'])
+# print(Direcoes(1).name, Direcoes['ESQUERDA'].name, Direcoes.ESQUERDA.value)
+
+def mover(direcao:Direcoes):
+    if not isinstance(direcao, Direcoes):
+        raise ValueError('Direção não encontrada')
+    print(f"Movendo para a direção {direcao.name}")
+
+mover(direcao.ESQUERDA)
+mover(direcao.DIREITA)    
+# mover("acima")
+# mover("abaixo")
