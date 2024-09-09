d4863353fb6569b7c4a38b228c509d36efc094dc
Rômulo Goes <romulo.fg99@gmail.com>
2024-09-09 19:55:02 -0300

    8º Commit - Herança em Python

---
+++ POO - Python/heranca.py
@@ -0,0 +1,25 @@
+'''POO em Python - Herança
+
+Herança é a relação mais íntima entre classes dentre as 4. 
+É quando a classe é outra. 
+
+Num pequeno resumo:
+Associação - usa
+Agregação - Tem
+Conposição - É dono
+Herança - É'''
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        self.nomeclasse = self.__class__.__name__
+
+    def falar(self):
+        print(f'{self.nomeclasse} Falando...')
+
+class Cliente(Pessoa):
+    pass
+
+class Aluno(Pessoa):
+    pass
---
+++ POO - Python/main_heranca.py
@@ -0,0 +1,7 @@
+from heranca import *
+
+c1 = Cliente('Luiz', 45)
+c1.falar()
+a1 = Aluno('Maria', 54)
+a1.falar()
+
